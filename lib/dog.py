9418e7498fd43d0a6f49bebea167f891a70a5be8
Bakeloh <bakelohnoel@gmail.com>
2024-02-07 14:32:21 +0300

    Commplet Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,13 @@
 
 class Dog:
     # Class body goes here
+    def sit(self):
+          print("Woof!")
+    
+# Create an instance of the class called "my_dog" with no arguments.
+    
 
     #Instance method definition
+    def back(self):
+        print("The dog is sitting.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,18 @@
 
 class Person:
     # Class body goes here
+    def sit(self):
+        print("Hello World!")
 
     #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+        
+# # Create an object of the class
+# person1 = Person()
+
+# # Call instance methods using dot notation
+# person1.sit()  # Output: Hello
+# person1.walk()  # Output: Walking...
+# pass
+
